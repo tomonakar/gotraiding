193d58b75b5b401f178ed16e7be8a1b1b8c7b84a
tomonakar <tomohisa.nak@gmail.com>
2020-04-21 13:05:36 +0900

    feat: add loggingSettings

---
+++ utils/logging.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+// LoggingSettings is set the log for project.
+func LoggingSettings(logFile string) {
+	logfile, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("file=logFile err=%s", err.Error())
+	}
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
+}
